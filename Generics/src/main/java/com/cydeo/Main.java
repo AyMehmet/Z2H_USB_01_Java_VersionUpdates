ba27b767aeca30cb0298640be4652c81691c7e25
aymehmet <aymehmet1976@gmail.com>
2022-08-18 18:51:22 +0300

    commit2

--- Generics/src/main/java/com/cydeo/Main.java
+++ Generics/src/main/java/com/cydeo/Main.java
@@ -1,4 +1,16 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList items=new ArrayList();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+
+    }
 }
