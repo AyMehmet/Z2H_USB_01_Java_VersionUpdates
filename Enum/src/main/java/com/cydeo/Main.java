478120117c5cd7560f6326e942c2f3c2ff951a7b
aymehmet <aymehmet1976@gmail.com>
2022-08-19 22:23:56 +0300

    Currency created

---
+++ Enum/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("how to access  a constant");
+
+        Currency c=Currency.DIME;
+        System.out.println(c);
+    }
+}
